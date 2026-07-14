5fd0340e362f1954002fa5f43ee11423d49d529f
Abdullah <malikkarim0100@gmail.com>
2026-07-14 13:35:45 +0700

    Menambahkan isi seeder

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -2,8 +2,9 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Admin;
+use Illuminate\Support\Facades\Hash;
 
 class AdminSeeder extends Seeder
 {
@@ -12,6 +13,9 @@ class AdminSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Admin::create([
+            'username' => 'admin',
+            'password' => Hash::make('admin123'),
+        ]);
     }
 }
--- database/seeders/KategoriMobilSeeder.php
+++ database/seeders/KategoriMobilSeeder.php
@@ -2,8 +2,8 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Kategori_mobil;
 
 class KategoriMobilSeeder extends Seeder
 {
@@ -12,6 +12,14 @@ class KategoriMobilSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        Kategori_mobil::create(['nama_kategori' => 'SUV']);
+
+        Kategori_mobil::create(['nama_kategori' => 'MPV']);
+
+        Kategori_mobil::create(['nama_kategori' => 'Sedan']);
+
+        Kategori_mobil::create(['nama_kategori' => 'Hatchback']);
+        
+        Kategori_mobil::create(['nama_kategori' => 'Pickup']);
     }
 }
